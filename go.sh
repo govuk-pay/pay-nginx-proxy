6cd3cf4b7d64eb6ea0be99b7884c4078637b0387
Joe Roberts <joe.roberts@digital.cabinet-office.gov.uk>
2026-09-15 14:28:55 +0100

    [PP-15916] Use post-quantum key exchange if client supports it

--- go.sh
+++ go.sh
@@ -23,6 +23,8 @@ cat > /etc/nginx/conf/server_certs.conf <<-EOF_CERT_CONF
     # And make sure we prefer them
     ssl_ciphers ECDHE-ECDSA-AES256-GCM-SHA384:ECDHE-RSA-AES256-GCM-SHA384:ECDHE-ECDSA-CHACHA20-POLY1305:ECDHE-RSA-CHACHA20-POLY1305:ECDHE-ECDSA-AES128-GCM-SHA256:ECDHE-RSA-AES128-GCM-SHA256:ECDHE-ECDSA-AES256-SHA384:ECDHE-RSA-AES256-SHA384:ECDHE-ECDSA-AES128-SHA256:ECDHE-RSA-AES128-SHA256:DHE-RSA-AES256-GCM-SHA384:DHE-RSA-AES128-GCM-SHA256;
     ssl_prefer_server_ciphers on;
+    # Prefer to us PQC key exchange method if client supports it, otherwise TLS 1.3 defaults
+    ssl_ecdh_curve X25519MLKEM768:X25519:prime256v1:secp384r1;
     ssl_session_cache shared:SSL:10m;
     ssl_session_timeout 10m;
     ssl_stapling ${SSL_STAPLING:-on};
